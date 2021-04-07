d98f2988e8da3acf4ae5f9d07cedab205fec35eb
Flavio Lopes <flaviotxa@gmail.com>
2021-04-07 21:38:20 +0100

    Implements URL Placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,9 @@ export default function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={ <Products />} />
-            <Route path="/detail" element={ <Detail />} />
+            <Route path="/" element={ <h1>Welcome to Carved Rock Fitness</h1>} />
+            <Route path="/:category" element={ <Products />} />
+            <Route path="/:category/:id" element={ <Detail />} />
             <Route path="/cart" element={ <Cart />} />
           </Routes>
         </main>
