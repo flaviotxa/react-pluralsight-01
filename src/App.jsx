48b2a875dc9148a0f748ecf65b0ebed89d177d95
Flavio Lopes <flaviotxa@gmail.com>
2021-04-08 23:05:49 +0100

    Implements cart state using localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,7 +8,16 @@ import Detail from "./Detail";
 import Cart from "./Cart";
 
 export default function App() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? [];
+    } catch {
+      console.error("The cart could not be parsed into JSON.");
+      return [];
+    }
+  });
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
 
   function addToCart(id, sku) {
     setCart((items) => {
@@ -25,7 +34,7 @@ export default function App() {
 
   function updateQuantity(sku, quantity) {
     setCart((items) => {
-      return (quantity === 0) 
+      return quantity === 0
         ? items.filter((i) => i.sku !== sku)
         : items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
     });
