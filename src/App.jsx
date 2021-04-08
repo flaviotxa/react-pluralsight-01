ef47fc7c3a51765873f1050914e313274c077117
Flavio Lopes <flaviotxa@gmail.com>
2021-04-08 22:45:57 +0100

    Implements cart component and logic to choose quantity

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,12 @@ export default function App() {
     });
   }
 
+  function updateQuantity(sku, quantity) {
+    setCart((items) => {
+      return items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+    });
+  }
+
   return (
     <>
       <div className="content">
@@ -31,8 +37,11 @@ export default function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>} />
-            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/:category/:id"
+              element={<Detail addToCart={addToCart} />}
+            />
+            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>} />
           </Routes>
         </main>
       </div>
